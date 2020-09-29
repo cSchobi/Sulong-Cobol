0d6ed1100ce871143fc7e6921f339f6bce3cf349
Christoph <c.schobesberger@gmx.net>
2020-09-29 15:34:44 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-export GRAALVM_PATH=/home/christoph/graalvm-ee-java8-20.3.0-dev
-export PATH=$GRAALVM_PATH/bin:$PATH # export path for lli
+export GRAALVM_PATH=/home/christoph/graalvm-ee-java8-20.2.0
+# export path for lli
+export PATH=$GRAALVM_PATH/bin:$PATH 
 export LLVM_TOOLCHAIN=$(lli --llvm.managed --print-toolchain-path)
-export PATH=$LLVM_TOOLCHAIN:$PATH # set path for gcc
+# set path for gcc
+export PATH=$LLVM_TOOLCHAIN:$PATH 
 
 # path to graalvm managed libraries
 export GRAALVM_LIBRARIES_PATH=$GRAALVM_PATH/jre/languages/llvm/managed/lib
