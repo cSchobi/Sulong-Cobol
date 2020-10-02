dd07a1e668e18cd27e9b9d911b65e39cb2183f82
Christoph <c.schobesberger@gmx.net>
2020-10-02 10:50:45 +0200

    refactor javascript function

--- cobsha3/SHA3-256.js
+++ cobsha3/SHA3-256.js
@@ -4,24 +4,24 @@ var cobol = Polyglot.evalFile("llvm", "KECCAK.so");
 
 (function SHA3_256(sha_input, sha_input_length, sha_output) {
 
+    cobol.KECCAK__Wrapper__struct({
+        LNK_KECCAK_RATE: 1088,
+        LNK_KECCAK_CAPACITY: 512,
+        LNK_KECCAK_INPUT: sha_input,
+        LNK_KECCAK_INPUT_BYTE_LEN: sha_input_length,
+        LNK_KECCAK_DELIMITED_SUFFIX: 0x06,
+        LNK_KECCAK_OUTPUT: sha_output,
+        LNK_KECCAK_OUTPUT_BYTE_LEN: 32
+    }); 
+
+    /*     
+    // inform GnuCOBOl how many arguments are used
+    libcob.set_cob_call_parameters(7);
+
     var keccak_rate = 1088;
     var keccak_capacity = 512;
     var keccak_delimited_suffix = 0x06;
     var output_byte_len = 32;
-
-    cobol.KECCAK__Wrapper__struct({
-        LNK_KECCAK_RATE: keccak_rate,
-        LNK_KECCAK_CAPACITY: keccak_capacity,
-        LNK_KECCAK_INPUT: sha_input,
-        LNK_KECCAK_INPUT_BYTE_LEN: sha_input_length,
-        LNK_KECCAK_DELIMITED_SUFFIX: keccak_delimited_suffix,
-        LNK_KECCAK_OUTPUT: sha_output,
-        LNK_KECCAK_OUTPUT_BYTE_LEN: output_byte_len
-    }); 
-
-/*     
-    // inform GnuCOBOl how many arguments are used
-    libcob.set_cob_call_parameters(7);
 
     cobol.KECCAK__Wrapper(
         keccak_rate,
